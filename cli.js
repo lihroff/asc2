dff25f9373e1dcc03881169e46cc8b066bd90d03
TommY Lee <lihroff@icloud.com>
2021-04-15 15:42:35 +0800

    fix: Oct and Hex value

--- cli.js
+++ cli.js
@@ -10,9 +10,12 @@ const cli = meow(
 	  $ asc2 
 
 	Options
-    --dec, -d ASCII digital code.
-    --char, -c ASCII char code.
-    --string, -s Iterate each character. 
+    --dec, -d find ASCII with digital code.
+    --char, -c find ASCII with char code.
+    --oct, -o find ASCII with oct code.
+    --hex, -h find ASCII with hex code.
+    --bin, -b find ASCII with binary code.
+    --string, -s Iterate each character ASCII . 
 
 	Examples
 	  $ asc2 -c A
@@ -26,6 +29,18 @@ const cli = meow(
       char: {
         type: 'string',
         alias: 'c'
+      },
+      oct: {
+        type: 'string',
+        alias: 'o'
+      },
+      hex: {
+        type: 'string',
+        alias: 'h'
+      },
+      bin: {
+        type: 'string',
+        alias: 'b'
       },
       string: {
         type: 'string',
@@ -42,22 +57,51 @@ if (_.isEmpty(opt)) {
   return;
 }
 
-if (opt.char) {
-  console.log(_.find(item => item.Char === opt.char.charAt(), AsciiTable).toString());
-  return;
-}
-
 if (opt.string) {
-  [...opt.string].forEach(c =>
-    console.log(_.find(item => item.Char === c.charAt(), AsciiTable).toString())
-  );
+  [...opt.string].forEach(c => {
+    const char = _.find(item => item.Char === c, AsciiTable);
+    if (char) {
+      char.print();
+    } else {
+      console.log(`> Not an Ascii char: ${c}`);
+    }
+  });
   return;
 }
 
 if (opt.dec) {
   const char = AsciiTable[opt.dec];
   if (char) {
-    console.log(char.toString());
+    char.print();
   }
+  return;
+}
+
+if (opt.oct) {
+  const char = _.find(item => +item.Dec === Number.parseInt(opt.oct, 8), AsciiTable);
+  if (char) {
+    char.print();
+  }
+  return;
+}
+
+if (opt.hex) {
+  const char = _.find(item => +item.Dec === Number.parseInt(opt.hex, 16), AsciiTable);
+  if (char) {
+    char.print();
+  }
+  return;
+}
+
+if (opt.bin) {
+  const char = _.find(item => +item.Dec === Number.parseInt(opt.bin, 2), AsciiTable);
+  if (char) {
+    char.print();
+  }
+  return;
+}
+
+if (opt.char) {
+  _.find(item => item.Char === opt.char[0], AsciiTable).print();
   return;
 }
